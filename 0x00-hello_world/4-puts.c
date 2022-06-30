7c818a6bae3acf37f2c1d51077449d5ab79d3d24
KatuaM <mutheuannette81@gmail.com>
2022-06-30 19:01:48 +0000

    4th task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ *main - Entry point
+
+ * Using put to print to std output
+ */
+int main(void);
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
