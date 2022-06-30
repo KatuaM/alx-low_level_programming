7fac05c79dda119bf27f7c34e6cd989b0b61a8e9
KatuaM <mutheuannette81@gmail.com>
2022-06-30 19:32:06 +0000

    4th task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/*
+/**
  *main - Entry point
- * Using put to print to std output
+ *Return: Always 0 (Success)
  */
-int main(void);
-{The Program description
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
