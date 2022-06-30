52f88e3ca567a7b785c7c348a7407e64dbeecb8a
KatuaM <mutheuannette81@gmail.com>
2022-06-30 19:17:06 +0000

    hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /*
  *main - Entry point
-
  * Using put to print to std output
  */
 int main(void);
-{
+{The Program description
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
