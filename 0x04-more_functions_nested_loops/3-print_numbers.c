9cd43988161a4991a0c6677b2969bac6363e832b
KatuaM <101462009+KatuaM@users.noreply.github.com>
2022-07-08 21:28:01 +0300

    Update 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*print_numbers - function that prints the numbers
+*
+*/
+void print_numbers(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+	_putchar(x);
+	}
+	_putchar('\n');
+}
