6d6982880e66fef01bf35364a609db6e997cf742
KatuaM <101462009+KatuaM@users.noreply.github.com>
2022-07-08 21:18:10 +0300

    Update 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_isdigit - checks for a digit (0 through 9)
+*@c: displays digits
+* Return: 1 if character is digit and 0 for otherwise
+*/
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+}
+	_putchar('\n');
+}
