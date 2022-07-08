1a4f43a75abde01176bb50de3fccce35ac1fab12
KatuaM <101462009+KatuaM@users.noreply.github.com>
2022-07-08 21:31:35 +0300

    Update 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+*print_line - a function that draws a straight line in the terminal
+*@n: value of int
+*
+*/
+
+void print_line(int n)
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+	{
+	_putchar('_');
+	}
+	_putchar('\n');
+}
